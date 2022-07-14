a26a448a7d9e496524b82df4530652529ff12d44
Damian Barrous-Dume <damianbarrous@gmail.com>
2022-07-14 23:01:21 +0100

    Make cdk change

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from cdk_deployment.sdc_aws_processing_lambda import SDCAWSProcessingLambdaStack
 app = cdk.App()
 SDCAWSProcessingLambdaStack(app, "SDCAWSProcessingLambdaStack", env=cdk.Environment(
         account=os.getenv('CDK_DEFAULT_ACCOUNT'),
-        region=os.getenv('CDK_DEFAULT_REGION')
+        region='us-east-2'
     )
 )
 
--- cdk_deployment/sdc_aws_processing_lambda.py
+++ cdk_deployment/sdc_aws_processing_lambda.py
@@ -2,35 +2,25 @@ from aws_cdk import (
     Stack,
     aws_lambda,
     aws_ecr,
-    aws_ecr_assets,
-    aws_iam as iam,
 )
-import json
 from constructs import Construct
 import logging
-import os
 class SDCAWSProcessingLambdaStack(Stack):
 
    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
+        
+        # ECR Repo Name
         repo_name = 'sdc_aws_processing_function'
 
-        ecr_repository = aws_ecr.Repository(
+        # Get SDC Processing Lambda ECR Repo
+        ecr_repository = aws_ecr.Repository.from_repository_name(
                             self, 
                             id=f"{repo_name}_repo", 
                             repository_name=repo_name,
                             )
         
-        login_ecr_command = f"aws ecr get-login-password --region {os.getenv('CDK_DEFAULT_REGION')} | docker login --username AWS --password-stdin {ecr_repository.repository_uri_for_tag()}"
-        docker_build_command = f'cd function && docker build -t {repo_name}:latest .'
-        docker_tag_command = f"docker tag sdc_aws_processing_lambda:latest {ecr_repository.repository_uri_for_tag()}/sdc_aws_processing_lambda:latest"
-        # push_ecr_command = f""
-        # docker_tag_command = 
-        os.system(login_ecr_command)
-        os.system(docker_build_command)
-        logging.warning(login_ecr_command)
-        ### Create Cognito Remediator Lambda function
+        # Create Container Image ECR Function 
         sdc_aws_processing_function = aws_lambda.DockerImageFunction(
             scope=self,
             id=f"{repo_name}_function",
@@ -38,6 +28,8 @@ class SDCAWSProcessingLambdaStack(Stack):
             description="SWSOC Processing Lambda function deployed using AWS CDK Python",
             code=aws_lambda.DockerImageCode.from_ecr(ecr_repository),
         )
+
+        logging.info("Function created successfully: %s", sdc_aws_processing_function)
 
 
 
