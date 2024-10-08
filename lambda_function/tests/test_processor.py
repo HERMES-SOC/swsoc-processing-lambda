04a78572ed0bfe00dbad22e59ac5f12b6394ed36
Damian Barrous-Dume <damianbarrous@gmail.com>
2024-10-08 11:01:57 +0100

    fix-tests

--- lambda_function/tests/test_processor.py
+++ lambda_function/tests/test_processor.py
@@ -3,7 +3,7 @@ import os
 import json
 from moto import mock_s3
 from pathlib import Path
-import astropy.logger
+from astropy import log
 
 os.environ["SDC_AWS_CONFIG_FILE_PATH"] = "lambda_function/src/config.yaml"
 from src.file_processor.file_processor import (  # noqa: E402
@@ -11,7 +11,7 @@ from src.file_processor.file_processor import (  # noqa: E402
     FileProcessor,  # noqa: E402
 )  # noqa: E402
 
-astropy.logger.disable_warnings_logging()
+log.disable_warnings_logging()
 
 
 @pytest.fixture
